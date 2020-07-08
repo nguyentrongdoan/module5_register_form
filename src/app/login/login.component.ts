29990dc7bc81e922ce0024d566e22167dd5a679e
nguyentrongdoan <nguyendoan206@gmail.com>
2020-07-08 08:52:24 +0700

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent implements OnInit {
+  loginForm: FormGroup;
+
+  constructor(private sb: FormBuilder) {
+    this.loginForm = this.sb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]]
+    });
+  }
+
+  ngOnInit(): void {
+  }
+  // tslint:disable-next-line:typedef
+  onSubmit(loginForm) {
+    console.log(loginForm);
+  }
+
+}
